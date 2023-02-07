3cc994d590c7024e033269b02c06ec872a4094d9
Neodevils <neoaichan@gmail.com>
2023-02-07 23:42:28 +0300

    My head hurts. Gonna work for it later lmao

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-// Listeners
+// Functions
 export { ping } from "./Functions/Info/ping.js";
 export { log } from "./Functions/Bot/log.js";
+
+// Classes
 export { Blehh } from "./Classes/Blehh.js";
+export { SlashCommand } from './Classes/SlashCommand.js'
 export { InteractionCreate } from "./Events/InteractionCreate.js";
 export { client } from "./index.test.js";
