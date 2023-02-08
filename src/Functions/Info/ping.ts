905960f9b686dfd8b886dcf25e83a7dab0f481fa
Liya <82108363+Slyrith@users.noreply.github.com>
2023-02-08 12:02:13 +0000

    Added shard on `ping()` and update readme

--- src/Functions/Info/ping.ts
+++ src/Functions/Info/ping.ts
@@ -1,20 +1,36 @@
-// This file is for ping function.
 import { client } from "../../index.js";
 
 /**
+ * 
  * @param typeOfPing The type of ping.
+ * @param shardId The id of the shard to get the ping from.
  * @returns Shows bot's ping.
  */
 
-async function ping(typeOfPing: "client" | "database") {
+async function ping(typeOfPing: "client" | "database", shardId?: number) {
+    let pingTime: number;
+
     if (typeOfPing === "client") {
-        return client.ws.ping;
+        if (shardId !== undefined && client.shard) {
+            const shard = client.shard.get(shardId);
+            if (shard) {
+                pingTime = shard.latency;
+            } else {
+                return `Shard with id ${shardId} doesn't exist.`;
+            }
+        } else if (client.shard) {
+            pingTime = client.shard.averageLatency;
+        } else {
+            pingTime = client.ws.ping;
+        }
     } else if (typeOfPing === "database") {
         // We will use the database later.
-        return client.db.ping;
+        pingTime = client.db.ping;
     } else {
         return "Invalid type provided. Please provide either 'client' or 'database'.";
     }
+
+    return `${typeOfPing} ping: ${pingTime}ms`;
 }
 
 // Exporting ping function.
