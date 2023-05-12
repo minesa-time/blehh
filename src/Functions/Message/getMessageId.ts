021d7c56a54e6cbe62d068552f57377c59dbd28f
Neo <71941576+Neodevils@users.noreply.github.com>
2023-05-12 19:45:44 +0000

    Added an error message.

--- src/Functions/Message/getMessageId.ts
+++ src/Functions/Message/getMessageId.ts
@@ -6,6 +6,9 @@ import { Message } from "discord.js";
  * @returns The ID of the message.
  */
 
-export function getMessageId(message: Message | any) {
+export function getMessageId(message: Message) {
+    if (!message) {
+        throw new Error("You forgot to add a message to get the ID of.");
+    }
     return message.id;
 }
