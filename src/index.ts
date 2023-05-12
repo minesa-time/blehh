c5e8313c25831373c4d4e47e40fbe38165b07161
Neo <71941576+Neodevils@users.noreply.github.com>
2023-05-12 19:53:32 +0000

    Deleted unused event and class

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,4 @@ export { getMessageId } from "./Functions/Message/getMessageId.js";
 
 // Classes
 export { Blehh } from "./Classes/Blehh.js";
-export { SlashCommand } from "./Classes/SlashCommand.js";
-export { InteractionCreate } from "./Events/InteractionCreate.js";
 export { client } from "./index.test.js";
